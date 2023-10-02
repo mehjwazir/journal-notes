fb57a1a232bc295c4f93a76b6158c9191ee16b1b
mehjwazir <mehjwazir@gmail.com>
2023-10-02 02:54:21 -0700

    in package.json added npm i dotenv, npm i mongoose, npm i envalid also update in package-lock created validateEnv.ts file and included code to clean env refer to code and in server.ts updated process.env to env.PORT also added imports dotenv and validateEnv and mongoose

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,26 @@
+import 'dotenv/config';
+import env from './util/validateEnv';
+import mongoose from 'mongoose';
 import express from 'express';
 const app = express();
-const port = 5000;
+
 
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
 
-app.listen(port, () => {
-	console.log("Server running on port : " + port);
-});
+
+const port = env.PORT;
+
+
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+	.then(() => {
+		console.log("Mongoose connected");
+		app.listen(port, () => {
+			console.log("Server running on port : " + port);
+		});
+	})
+	.catch(console.error);
+
+
+
