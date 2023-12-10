c4eb962da2854f9b42134bdb0cca113a9433697c
mehjwazir <mehjwazir@gmail.com>
2023-12-09 16:04:01 -0800

    added notes.ts file in controllers folder and routes folder added code to app.ts in util folder.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,26 @@
 import 'dotenv/config';
-import express from 'express';
-import NoteModel from './models/note'
+import express, { NextFunction, Request, Response } from 'express';
+import notesRoutes from "./routes/notes";
 
 
 const app = express();
 
-app.get("/", async (req, res) => {
+app.use(express.json());
 
-	try {
-		throw Error("Bazinga!");
-		const notes = await NoteModel.find().exec();
-		res.status(200).json(notes);
-	} catch (error) {
+app.use("/api/notes", notesRoutes);
+
+app.use((req, res, next) => {
+	next(Error("Endpoint not found"));
+});
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars 
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
 		console.error(error);
 		let errorMessage = "An unknown error occurred";
 		if (error instanceof Error) errorMessage = error.message;
 		res.status(500).json({ error: errorMessage });
-	}
+	
 
 });
 
