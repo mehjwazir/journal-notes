f4093d8721e793f75a093b7bf4188b3a04d2e950
mehjwazir <mehjwazir@gmail.com>
2023-12-10 23:26:05 -0800

    added router.patch end point in routes added interface UpdateNoteParams and UpdateNoteBody with noteId string and title text string with request handler and argument with params plus variables for noteId newTitle and newText with https error handling tested in postman

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -15,7 +15,7 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 
 export const getNote: RequestHandler = async (req, res, next) => {
 	const noteId = req.params.noteId;
-	
+
 	try {
 
 		if (!mongoose.isValidObjectId(noteId)) {
@@ -23,7 +23,7 @@ export const getNote: RequestHandler = async (req, res, next) => {
 		}
 
 		const note = await NoteModel.findById(noteId).exec();
-		
+
 		if (!note) {
 			throw createHttpError(404, "Note not found");
 		}
@@ -60,3 +60,49 @@ export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknow
 		next(error);
 	}
 }
+
+interface UpdateNoteParams {
+	noteId: string,
+}
+
+
+interface UpdateNoteNoteBody {
+	title?: string,
+	text?: string,
+}
+
+
+export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteNoteBody, unknown> = async (req, res, next) => {
+
+	const noteId = req.params.noteId;
+	const newTitle = req.body.title;
+	const newText = req.body.text;
+
+
+
+	try {
+		if (!mongoose.isValidObjectId(noteId)) {
+			throw createHttpError(400, "Invalid note id");
+		}
+		if (!newTitle) {
+			throw createHttpError(400, "Note must have a title");
+		}
+
+		const note = await NoteModel.findById(noteId).exec();
+
+		if (!note) {
+			throw createHttpError(404, "Note not found");
+		}
+
+		note.title = newTitle;
+		note.text = newText;
+
+		const updatedNote = await note.save();
+
+		res.status(200).json(updatedNote);
+
+	} catch (error) {
+		next(error);
+
+	}
+};
