a00413de7b231ec3355c4c6bd5d299c96fb4302f
mehjwazir <mehjwazir@gmail.com>
2023-12-11 11:26:10 -0800

    added delete note in controllers and routes

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -106,3 +106,24 @@ export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteNot
 
 	}
 };
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+	const noteId = req.params.noteId;
+	try {
+		if (!mongoose.isValidObjectId(noteId)) {
+			throw createHttpError(400, "Invalid note id");
+		}
+
+		const note = await NoteModel.findById(noteId).exec();
+
+		if (!note) {
+			throw createHttpError(404, "Note not found");
+		}
+
+		await note.deleteOne();
+
+		res.sendStatus(204);
+	} catch (error) {
+		next(error);
+	}
+};
