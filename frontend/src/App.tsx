b2e138e5add8030fef343b1d385a703fa7436075
mehjwazir <mehjwazir@gmail.com>
2023-12-11 23:53:21 -0800

    added showAddNoteDialog and set the useState to false imported AddNoteDialog added Button imported button for the setShowAddNoteDialog to true added AddnoteDialog.tsx in components folder added interface AddNoteDialogProps onDismiss destructered AddNoteDialog with onDismiss and AddNoteDialogProps added Modal Modal.Header and Model.Title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Note as NoteModel} from './models/note';
 import Note from './components/Note';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Button } from 'react-bootstrap';
 import styles from "/styles/NotesPage.module.css"
 import * as NotesApi from "./network/notes_api";
+import AddNoteDialog from './components/AddNoteDialog';
 
 function App() {
 
   const [notes, setNotes] = useState<NoteModel[]>([]);
+
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
 
   useEffect(() => {
     async function loadNotes() {
@@ -26,15 +29,24 @@ function App() {
 
   return (
     <Container>
-      <Row xs={1} md={2} x1={3} className='g-4'>
+      <Button
+        onClick={() => setShowAddNoteDialog(true)}>
+        Add new note
+</Button>
 
+      <Row xs={1} md={2} x1={3} className='g-4'>
         {notes.map(note => (
           <Col key={note._id} >
             <Note note={note} className={styles.note} />
         </Col>
      
       ))}
-        </Row>
+      </Row>
+      {showAddNoteDialog &&
+        <AddNoteDialog 
+        onDismiss={() => setShowAddNoteDialog(false)}
+      />
+      }
     </Container>
   );
 }
