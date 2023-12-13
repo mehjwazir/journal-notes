95a70c9ea62dbcfbbb09c1b738589dd40c5cd407
mehjwazir <mehjwazir@gmail.com>
2023-12-12 21:45:55 -0800

    added Modal.Body in AddNoteDialog.tsx with Form FormGroup FormLabel FormControl Modal Footer and Button added npm i react-hook-form which was updated in package.json and lock as a package.

--- frontend/src/components/AddNoteDialog.tsx
+++ frontend/src/components/AddNoteDialog.tsx
@@ -1,4 +1,4 @@
-import { Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 
 
 interface AddNoteDialogProps {
@@ -14,6 +14,36 @@ const AddNoteDialog = ({onDismiss}: AddNoteDialogProps) => {
 			</Modal.Title>
 				
 			</Modal.Header>
+			<Modal.Body>
+				<Form id="addNoteForm">
+					<Form.Group className="mb-3">
+						<Form.Label>Title</Form.Label>
+						<Form.Control 
+							type="text"
+							placeholder="Title"
+						/>
+
+					</Form.Group>
+					<Form.Group className="mb-3">
+						<Form.Label>Text</Form.Label>
+						<Form.Control
+							as="textarea"
+							rows={5}
+							placeholder="Text"
+
+						/>
+					</Form.Group>
+				</Form>
+			</Modal.Body>
+
+			<Modal.Footer>
+				<Button
+					type="submit"
+					form="addNoteForm"
+				>
+					Save
+				</Button>
+			</Modal.Footer>
 		</Modal>
 	 );
 }
