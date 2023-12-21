ce5dae4584ad49ad91019a646ba0df279b3bb29f
mehjwazir <mehjwazir@gmail.com>
2023-12-21 15:00:03 -0800

    added existingUsername and existingEmail variables with the UserModel imported UserModel from models and created conditional HttpErrors in users.ts in controllers folder

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,8 +1,9 @@
 import { RequestHandler } from "express";
 import createHttpError from "http-errors";
+import UserModel from "../models/user";
 
 interface SignUpBody {
-	username?: string, 
+	username?: string,
 	email?: string,
 	password?: string,
 }
@@ -15,7 +16,21 @@ export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = asy
 	try {
 		if (!username || !email || !passwordRaw) {
 			throw createHttpError(400, "Parameters missing");
-	}
+		}
+
+		const existingUsername = await UserModel.findOne({ username: username }).exec();
+
+		if (existingUsername) {
+			throw createHttpError(409, "Username already taken. Please choose a different one or log in instead.");
+		}
+
+		const existingEmail = await UserModel.findOne({ email: email }).exec()
+		
+		if (existingEmail) {
+			
+			throw createHttpError(400, "A user with this email already exists. Please log in instead");
+		}
+
 	} catch (error) {
 		next(error);
 	}
