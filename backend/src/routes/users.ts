168023dce5cd3f8186c12f0383c710aaab818255
mehjwazir <mehjwazir@gmail.com>
2023-12-22 23:13:46 -0800

    added getAuthenticatedUser function with requesthandler and try catch block for createhttperrors in users.ts in controllers added router.get home route for getAuthenticated user in users.ts in routers

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -4,6 +4,8 @@ import * as UserController from "../controllers/users";
 
 const router = express.Router();
 
+router.get("/", UserController.getAuthenticatedUser);
+
 router.post("/signup", UserController.signUp);
 
 router.post("/login", UserController.login);
