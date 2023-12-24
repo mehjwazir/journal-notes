6974f737e9c257261661154140813c1eafe82409
mehjwazir <mehjwazir@gmail.com>
2023-12-24 03:06:20 -0800

    added signUp and login async functions with interfaces signUpCredentials and LoginCredentials in notes_api.ts

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -20,6 +20,43 @@ export async function getLoggedInUser(): Promise<User> {
 }
 
 
+export interface SignUpCredentials {
+	username: string,
+	email: string,
+	password: string,
+}
+
+export async function signUp(credentials: SignUpCredentials): Promise<User> {
+	const response = await fetchData("/api/users/signup",
+		{
+		
+			method: "POST",
+			headers: {
+				"Content - Type": "application/json",
+			},
+			body: JSON.stringify(credentials),
+		});
+	return response.json();
+}
+
+export interface LoginCredentials {
+	username: string,
+	password: string,
+}
+
+export async function login(credentials: SignUpCredentials): Promise<User> {
+	const response = await fetchData("/api/users/login",
+		{
+
+			method: "POST",
+			headers: {
+				"Content - Type": "application/json",
+			},
+			body: JSON.stringify(credentials),
+		});
+	return response.json();
+}
+
 
 	export async function fetchNotes(): Promise<Note[]> {
 		const response = await fetchData("/api/notes", { method: "GET" });
