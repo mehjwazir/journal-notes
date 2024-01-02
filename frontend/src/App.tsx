e56605f73e50fab6054dc486f6998b2d4ba80939
mehjwazir <mehjwazir@gmail.com>
2024-01-02 13:17:33 -0800

    added NotesPageLoggedOutView file in components set up functional component added code in App.tsx with imports set states for loggedInUser SignUpModal and LoginModal. Added useEffect to fetchLoggedInUser and async try catch block to fetch notes api and fetchloggged in user data but only to render it once [] added states of setshow login, signup and loggedinuser added ternary in loggedinuser inside fragments. set up ondismiss and onsignup and login passing states

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,37 +2,73 @@ import { Container } from 'react-bootstrap';
 import LoginModal from './components/LoginModal';
 import NavBar from './components/NavBar';
 import SignUpModal from './components/SignUpModal';
-
+import { useEffect, useState } from 'react';
+import { User } from './models/user';
+import * as NotesApi from "./network/notes_api";
+import styles from "./styles/NotesPage.module.css"
+import NotesPageLoggedInView from './components/NotesPageLoggedInView';
+import NotesPageLoggedOutView from './components/NotesPageLoggedOutView';
 
 function App() {
+
+
+  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+
+  const [showSignUpModal, setShowSignUpModal] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
+
+  useEffect(() => {
+    async function fetchLoggedInUser() {
+      try {
+        const user = await NotesApi.getLoggedInUser();
+        setLoggedInUser(user);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchLoggedInUser();
+  }, [])
 
   return (
     <div>
       <NavBar
-        loggedInUser={null}
-        onLogInClicked={() => { }}
-        onSignUpClicked={() => { }}
-        onLogoutSuccessful={() => {}}
+        loggedInUser={loggedInUser}
+        onLogInClicked={() => setShowLoginModal(true)}
+        onSignUpClicked={() => setShowSignUpModal(true)}
+        onLogoutSuccessful={() => setLoggedInUser(null)}
 
 
       />
-      <Container className='{styles.notesPage'>
+      <Container className={styles.notesPage}>
 
-        {false &&
-          <SignUpModal
-            onDismiss={() => { }}
-            onSignUpSuccessful={() => { }}
-          />
-        }
-        {false &&
-          <LoginModal
-            onDismiss={() => { }}
-            onLoginSuccessful={() => { }}
-          />
-        }
-
-
+        <>
+          {loggedInUser
+            ? <NotesPageLoggedInView />
+            : <NotesPageLoggedOutView />
+          }
+        </>
       </Container>
+
+      {showSignUpModal &&
+        <SignUpModal
+          onDismiss={() => setShowSignUpModal(false)}
+          onSignUpSuccessful={(user) => {
+            setLoggedInUser(user);
+            setShowSignUpModal(false);
+          }}
+        />
+      }
+      {showLoginModal &&
+        <LoginModal
+          onDismiss={() => setShowLoginModal(false)}
+          onLoginSuccessful={(user) => {
+            setLoggedInUser(user);
+            setShowSignUpModal(false);
+
+          }}
+        />
+      }
     </div>
   );
 }
---
+++ frontend/src/components/NotesPageLoggedOutView.tsx
@@ -0,0 +1,11 @@
+
+
+
+
+const NotesPageLoggedOutView = () => {
+	return ( 
+		<p>Please login to see your notes</p>
+	 );
+}
+ 
+export default NotesPageLoggedOutView;
