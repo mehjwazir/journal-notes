10739e9500e30be98c5498d01bd4175ff7d19d82
mehjwazir <mehjwazir@gmail.com>
2024-01-02 20:20:46 -0800

    removed  authenticatedUserId from functional component also in try catch in users.ts in controllers. In models notes.ts added userId: type: Schema.Types.ObjectId, require: true in noteSchema. Added middleware folder with auth.ts added function requireAuth with request handler req, res, next  and if else. In routes added requireAuth for get path /

--- backend/src/models/note.ts
+++ backend/src/models/note.ts
@@ -2,6 +2,7 @@ import { InferSchemaType, model, Schema } from "mongoose";
 
 
 const noteSchema = new Schema({
+	userId: { type: Schema.Types.ObjectId, require: true },
 	title: { type: String, required: true },
 	text: { type: String, },
 	
