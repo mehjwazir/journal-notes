a0b076ef0512613b430a7fc690c5d58d8be7e2f1
mehjwazir <mehjwazir@gmail.com>
2024-01-03 12:48:44 -0800

    added h5 instead of p tage in NotesPageLoggedOutView.tsx also added bootstrap class of mt-5 for margin top added text-secondary for gray color also added div with a class to center and an img tag for img poster along with class to adjust height and width added classes to global.css and added img called journal notes in img folder in components also added all imports

--- frontend/src/components/NotesPageLoggedOutView.tsx
+++ frontend/src/components/NotesPageLoggedOutView.tsx
@@ -1,10 +1,18 @@
-
+import Img1 from "./img/Journal Notes.png"
 
 
 
 const NotesPageLoggedOutView = () => {
 	return ( 
-		<p>Please login to see your notes</p>
+		<>
+			<h5 className="mt-5 text-secondary">Please login to see your notes</h5>
+			<div className="jn-img-div"jn-img-div>
+				<img src={Img1} alt="journal-notes-img" className="jn-img"/>
+			</div>
+		</>
+	
+		
+		
 	 );
 }
  
