536057a4450fcc81bf734e718a9d07f0c5a51853
mehjwazir <mehjwazir@gmail.com>
2024-01-14 15:05:22 -0800

    removed cors code in app.ts in backend folder still testing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -55,10 +55,10 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
 	
 });
 
-app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
+// app.use(function (req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+// });
 
 export default app;
