6dd036037c621738cac45a517e161559fe79d6f6
mehjwazir <mehjwazir@gmail.com>
2024-01-14 21:03:05 -0800

    changed origin in app.ts backend folder to frontend origin testing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,7 @@ import env from "./util/validateEnv";
 const app = express();
 
 app.use(cors({
-	origin: 'https://journal-notes-backend.up.railway.app',
+	origin: 'https://journal-notes.up.railway.app',
 	credentials: true,
 	
 })
